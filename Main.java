1d33a4c73ca2dcdba80da8d369ba623f6ba72dc9
Priyanshu7644 <priyanshukr988@gmail.com>
2026-08-03 16:18:38 +0530

    fix error minor

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        
-        System.print("This is on ");
+
+        System.out.print("This is on ");
         System.out.println("the same line.");
-        
+
         int number = 2026;
         System.out.println("The current year is " + number);
     }
